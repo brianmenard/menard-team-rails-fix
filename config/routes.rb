5da9115f84435ea19fae90e426c5c8901f9e8408
Brian Menard <hello@webdevbrian.com>
2016-09-27 16:14:56 +0000

    created index route, view, and added index action to application controller.  Implemented bootsrap CDN into application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+get '/', to: 'application#index'
+
+end
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +64,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
